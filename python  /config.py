009c812e36e35d04998147890d2b3e4ea4297023
Papa Arona Ndiaye <ndiayearona0001@gmail.com>
2026-09-18 12:56:30 +0200

    Update config.py

--- python  /config.py
+++ python  /config.py
@@ -9,6 +9,9 @@ FIGURES = ROOT / "figures"
 NO2_FILE = RAW / "no2.csv"
 TRAFFIC_FILE = RAW / "traffic.gpkg"
 LANDUSE_FILE = RAW / "landuse.gpkg"
+CAMS_FILE = RAW / "cams.gpkg"
+BOUNDARY_FILE = RAW / "berlin_boundary.gpkg"
+
 STATIONS_FILE = ROOT / "stations" / "stations.csv"
 
 CLEAN_NO2 = PROCESSED / "no2_clean.csv"
@@ -20,29 +23,7 @@ VALIDATION = PROCESSED / "validation_results.csv"
 NO2_URL = "https://luftdaten.berlin.de/core/no2.csv"
 
 CRS = "EPSG:25833"
-IDW_POWER (p) = 2
+IDW_POWER(p)= 2
 
-CAMS_FILE = RAW / "cams.gpkg"
-
-TRAFFIC_BINS = [
-    20,
-    4500,
-    6540,
-    8620,
-    11080,
-    14160,
-    18700,
-    26080,
-    91080
-]
-
-TRAFFIC_LABELS = [
-    "20 - 4500",
-    "4500 - 6540",
-    "6540 - 8620",
-    "8620 - 11080",
-    "11080 - 14160",
-    "14160 - 18700",
-    "18700 - 26080",
-    "26080 - 91080"
-]
+PROCESSED.mkdir(parents=True, exist_ok=True)
+FIGURES.mkdir(parents=True, exist_ok=True)
